e29f62a7011f89d3bdc635095874a8a8c680e52c
César Rodríguez <kesarr@gmail.com>
2023-01-24 22:47:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from transformers import pipeline
+import PIL
+import requests
 import torch
+from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
+
+import base64
+from io import BytesIO
+import os
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
     
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model_id = "timbrooks/instruct-pix2pix"
+    model = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
+    model.to("cuda")
+    model.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+
+
+def download_image(url):
+    image = PIL.Image.open(requests.get(url, stream=True).raw)
+    image = PIL.ImageOps.exif_transpose(image)
+    image = image.convert("RGB")
+    return image
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -15,12 +30,17 @@ def inference(model_inputs:dict) -> dict:
     global model
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    prompt = model_inputs.get('prompt', 'blue skin')
+    image_url = model_inputs.get('url', 'https://avatars.githubusercontent.com/u/1288106?s=400&u=7a91d67df3f308c76ceda288237b150d1bf859d2&v=4')
+    image_downloaded = download_image(image_url)
     
     # Run the model
-    result = model(prompt)
+    images = model(prompt, image=image_downloaded, num_inference_steps=10, image_guidance_scale=1).images
 
     # Return the results as a dictionary
-    return result
+    buffered = BytesIO()
+    image.save(images[0],format="JPEG")
+    image_base64 = base64.b64encode(images[0].getvalue()).decode('utf-8')
+
+    # Return the results as a dictionary
+    return {'image_base64': image_base64}
