397895706b7d5b10cbbf6d72abca652e19f67ca5
César Rodríguez <kesarr@gmail.com>
2023-01-24 22:59:52 +0100

    Update download.py

--- download.py
+++ download.py
@@ -4,7 +4,7 @@ def download_model():
     model_id = "timbrooks/instruct-pix2pix"
     model = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
     model.to("cuda")
-    model.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    model.scheduler = EulerAncestralDiscreteScheduler.from_config(model.scheduler.config)
 
 if __name__ == "__main__":
     download_model()
